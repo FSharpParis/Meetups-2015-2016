4e37131d23468188a0cbd156efd71679f780dc1b
Pierre IRRMANN <pierre.irrmann@gmail.com>
2016-04-21 20:17:56 +0200

    A more complex sample

--- 2016-04-21/Sample.fsx
+++ 2016-04-21/Sample.fsx
@@ -10,9 +10,34 @@ let sample =
                     Text.Block "This is an introduction."
                     Text.Block "Let's try to render simple elements such as:"
                     Text.List [
+                        "Titled sections"
                         "Blocks"
                         "Lists"
                     ]
                 ] |> Section.FromParts }
-        
+
+        TitledSections [
+            {
+            Title = "Let's handle emphasis in paragraphs"
+            Section =
+                [
+                    Paragraph(
+                        Text [
+                            TextPart.Regular "Regular,"
+                            TextPart.Medium "Medium"
+                            TextPart.Strong "and Strong"
+                            TextPart.Regular "in the same paragraph."
+                        ])
+                ] |> Section.FromParts }
+            {
+            Title = "Let's handle emphasis in lists"
+            Section =
+                [
+                    List [
+                        Text.Regular "Regular"
+                        Text.Medium "Medium"
+                        Text.Strong "Strong"
+                    ]
+                ] |> Section.FromParts }
+        ]        
     ]
